4f71ce36762c80549b091729ff85be172f4df9a6
RKRedfern <rkredfern@gmail.com>
2021-05-07 15:52:05 -0400

    user controller updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,33 @@
-class Api::V1::UsersController < ApplicationController
+class API::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create, :index]
     
     def index
         users = User.all
-        render json: UserSerializer.new(users)
+        render json: users
+    end
+
+    def profile
+        render json: { user: UserSerializer.new(@user)}, status: :accepted
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        @user.destroy
+        render json: { success: 'User deleted successfully' }
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :password, :bio)
     end
 
 end
