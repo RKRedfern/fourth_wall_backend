73e83fdffbf50e2ae635f6a7e5c1e65be6b35ae2
RKRedfern <rkredfern@gmail.com>
2021-05-10 09:10:12 -0400

    auth controller added/routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+
       resources :houses
-      resources :ghosts
-      resources :users
-    end
+
+      resources :ghosts, only: [:show, :index, :create, :update]
+
+      resources :users, only: [:create, :index, :show, :destroy] do
+        resources :ghosts, only: [:index, :show, :create, :update, :destroy]
+      end
+
   end
   
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+
 end
